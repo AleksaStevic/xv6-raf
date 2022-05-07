b0b1853f13fe4ae5a431d80a4891439254b14392
Aleksa Stevic <aleksa.stevic26@gmail.com>
2022-05-08 00:10:06 +0200

    added user code for lsdel and rec software

--- user/lsdel.c
+++ user/lsdel.c
@@ -3,10 +3,22 @@
 #include "user.h"
 #include "kernel/fs.h"
 
+#define MAX_ROWS 64
+
 int
 main(int argc, char *argv[])
 {
-    printf("%s\n", "lsdel user software");
-    lsdel("", "");
+    // Matrica u kojoj treba da se smestaju nazivi obrisanih fajlova.
+    char df[MAX_ROWS][DIRSIZ+1];
+
+    int result = lsdel(argc < 2 ? "." : argv[1], (char *)df);
+    if (result < 0) {
+        printf("Navedena putanja nije validna.\n");
+    } else if (result == 0) {
+        printf("Nema obrisanih datoteka u navedenom direktorijumu.\n");
+    } else {
+        // @TODO: izlistaj fajlove iz df matrice
+        printf("lsdel user software.\n");
+    }
 	exit();
 }
--- user/rec.c
+++ user/rec.c
@@ -6,7 +6,20 @@
 int
 main(int argc, char *argv[])
 {
+    if (argc < 2) {
+        printf("Nije naveden parametar na komandnoj liniji.\n");
+        exit();
+        return 0;
+    }
+
     printf("%s\n", "rec user software");
-    rec("");
+    switch(rec(argv[1])) {
+        case 0:  printf("Uspesan oporavak.\n"); break;
+        case -1: printf("Roditeljski direktorijum za navedenu datoteku nije validan.\n"); break;
+        case -2: printf("Ne postoji obrisana datoteka sa navedenim nazivom.\n"); break;
+        case -3: printf("Inode datoteke je iskorišćen za nešto drugo.\n"); break;
+        case -4: printf("Neki blok datoteke je iskorišćen za nešto drugo.\n"); break;
+        default: printf("Nepoznata greska.\n");
+    }
 	exit();
 }
