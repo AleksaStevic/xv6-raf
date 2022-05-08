11452a8379c0a9c2ae9e7b59fb38a342648d2f9e
Aleksa Stevic <aleksa.stevic26@gmail.com>
2022-05-08 12:05:03 +0200

    finished lsdel user software

--- user/lsdel.c
+++ user/lsdel.c
@@ -9,16 +9,22 @@ int
 main(int argc, char *argv[])
 {
     // Matrica u kojoj treba da se smestaju nazivi obrisanih fajlova.
-    char df[MAX_ROWS][DIRSIZ+1];
+    char df[MAX_ROWS][DIRSIZ+1] = {
+        "file1",
+        "file2",
+        "file3",
+    };
 
-    int result = lsdel(argc < 2 ? "." : argv[1], (char *)df);
-    if (result < 0) {
+    //int fnum = lsdel(argc < 2 ? "." : argv[1], (char *)df);
+    int fnum = 3;
+    if (fnum < 0) {
         printf("Navedena putanja nije validna.\n");
-    } else if (result == 0) {
+    } else if (fnum == 0) {
         printf("Nema obrisanih datoteka u navedenom direktorijumu.\n");
     } else {
-        // @TODO: izlistaj fajlove iz df matrice
-        printf("lsdel user software.\n");
+        for (int i = 0; i < fnum; ++i) {
+            printf("%s\n", df[i]);
+        }
     }
 	exit();
 }
