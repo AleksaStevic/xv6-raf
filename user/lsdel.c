1da8b84ae28e3d3d75668f45ecce2165d95b53c8
Aleksa Stevic <aleksa.stevic26@gmail.com>
2022-05-08 14:19:40 +0200

    lsdel command finish

--- user/lsdel.c
+++ user/lsdel.c
@@ -7,14 +7,9 @@ int
 main(int argc, char *argv[])
 {
     // Matrica u kojoj treba da se smestaju nazivi obrisanih fajlova.
-    char df[DIRFNUM][DIRSIZ+1] = {
-        "file1",
-        "file2",
-        "file3",
-    };
+    char df[DIRFNUM][DIRSIZ+1];
 
-    //int fnum = lsdel(argc < 2 ? "." : argv[1], (char *)df);
-    int fnum = 3;
+    int fnum = lsdel(argc < 2 ? "." : argv[1], (char *)df);
     if (fnum < 0) {
         printf("Navedena putanja nije validna.\n");
     } else if (fnum == 0) {
